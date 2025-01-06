14034aeefad336c1ebdfd317c141a271cfca9cba
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-01-06 07:59:07 +0000

    Siste innspurt

--- StudentAdmin/obj/Debug/net8.0/StudentAdmin.AssemblyInfo.cs
+++ StudentAdmin/obj/Debug/net8.0/StudentAdmin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6984fb12bde7e59a2718a3b37aaad193d5ddb932")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98fea2b6c9242e2d161efdb446d7f46f30613b90")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
